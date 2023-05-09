5e7ef5ff59698a4d2f5d64dc84c385e0c69d8e3e
zxxng <jeayeong123@naver.com>
2023-05-09 11:45:36 +0900

    230509 Timer

---
+++ Timer.js
@@ -0,0 +1,13 @@
+const hoursInput = document.getElementById("hours");
+const minutesInput = document.getElementById("minutes");
+const secondsInput = document.getElementById("seconds");
+const startButton = document.getElementById("btn-start");
+const resetButton = document.getElementById("btn-reset");
+
+startButton.addEventListener('click', () => {
+
+})
+
+resetButton.addEventListener('click', () => {
+	
+})
